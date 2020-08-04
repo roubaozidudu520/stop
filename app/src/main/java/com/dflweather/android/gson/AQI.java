6d35b1fba2f142e796026af6693ce12f76a27a1e
rbzdd_glb <roubaozidudu520@aliyun.com>
2020-08-04 16:09:04 +0800

    weather

---
+++ app/src/main/java/com/dflweather/android/gson/AQI.java
@@ -0,0 +1,11 @@
+package com.dflweather.android.gson;
+
+public class AQI {
+
+    public AQiCity city;
+
+    public class AQiCity{
+        public String aqi;
+        public String pm25;
+    }
+}
---
+++ app/src/main/java/com/dflweather/android/gson/Forecast.java
@@ -0,0 +1,25 @@
+package com.dflweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Forecast {
+
+    @SerializedName("date")
+    public String date;
+
+    @SerializedName("tmp")
+    public Temperature temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class Temperature{
+        public String max;
+        public String min;
+    }
+
+    public class More{
+        @SerializedName("txt_d")
+        public String info;
+    }
+}
